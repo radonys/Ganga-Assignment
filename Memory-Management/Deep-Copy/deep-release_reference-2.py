52a9713f22ffb7bba083ef213bd7ddaf2346d5a1
Yash Srivastava <yashsrivastava3005@gmail.com>
2019-03-14 10:23:38 +0530

    Memory Management

---
+++ Memory-Management/Deep-Copy/deep-release_reference-2.py
@@ -0,0 +1,33 @@
+import copy
+import time
+from memory_profiler import profile
+
+@profile
+def deepcopy(i,simple_object):
+
+	simple_object.append(copy.deepcopy(simple_object[i-1]))
+
+	return simple_object
+
+@profile
+def remove_reference(simple_object):
+
+	del simple_object[0]
+
+	return simple_object
+
+n = 5
+simple_object = [[1,2,[3,4],5]]
+
+for j in range(100000):
+	simple_object[0].append([6])
+
+for i in range(1,n):
+
+	deepcopy(i,simple_object)
+	time.sleep(5)
+
+for i in range(0,n):
+
+	remove_reference(simple_object)
+	time.sleep(5)
