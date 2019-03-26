207f623784bd40c14518fc87bb2a9f204be2a761
Yash Srivastava <yashsrivastava3005@gmail.com>
2019-03-26 17:01:03 +0530

    Suggested Changes

--- Task/adder.py
+++ Task/adder.py
@@ -4,20 +4,18 @@ import re
 def mergefiles(file_list,output_file):
 
       try:
-            f_out = file(output_file,'w')
             sum_value = 0
 
             for f in file_list:
 
-                  f_in = file(f)
-                  numbers = re.findall(r"[-+]?\d*\.\d+|\d+", f_in.read())
-                  sum_value += int(numbers[0])
-                  print(int(numbers[0]))
-                  f_in.close()
+                  with open(f) as f_in:
+                  	
+                  	numbers = re.findall(r"[-+]?\d*\.\d+|\d+", f_in.read())
+                  	sum_value += int(numbers[0])
+                  	#print(int(numbers[0]))
 
-            f_out.write(str(sum_value))
-            f_out.flush()
-            f_out.close()
+            with open(output_file,'w') as f_out:
+            	f_out.write(str(sum_value))
 
             return True
 
--- Task/execute.sh
+++ Task/execute.sh
@@ -3,7 +3,7 @@
 execute()
 {
 	pdf2txt.py $1 > output.txt
-	echo $(tr -s ' ' '\n' < output.txt | grep the | wc -l)
+	echo $(egrep -ow 'the|The' output.txt | wc -l)
 }
 
 execute $1
